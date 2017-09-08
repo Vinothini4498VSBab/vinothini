aaadef0f0b90893171a019b82f125cfa113e7aa0
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2017-09-08 17:41:20 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int base,exponent,result=1;
+scanf("%d",&base);
+scanf("%d",exponent);
+while(exponent!=0)
+{
+result *=base
+--exponent;
+}
+scanf("%d",&result);
+}
