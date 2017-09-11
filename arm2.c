2b6c114319ef3509f2031bf0fdfba13701a423ab
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2017-09-11 17:54:36 +0530

    Create arm2.c

---
+++ arm2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,i,temp,rem;
+scanf("%d %d",&n1,&n2);
+for(i=n1+1;i<n2;i++)
+{
+temp=i;
+num=o;
+while(temp!=0)
+{
+rem=temp%10;
+num+=rem*rem*rem;
+temp=temp/10;
+}
+if(i==num)
+{
+printf9"%d",i);
+}
+]
+}
