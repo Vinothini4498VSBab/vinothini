1fd2ddad3cb3c06b62c68b455edf82dc4ef07f84
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2017-09-11 18:11:33 +0530

    Create prime2.c

---
+++ prime2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int checkprime(int n)
+void main()
+{
+int n1,n2,i,flag;
+scanf("%d %d",&n1,&n2);
+for(i=n1=1;i<n2;i++)
+{
+flag=checkprime(i)
+if(flag==1)
+{
+printf("%d",&i);
+}
+int checkprime(int n)
+{
+int j,flag=1;
+for(j=2;j<=n/2;++j)
+{
+if(n%j==0)
+{
+flag=0;
+break;
+}
+return flag;
+}
+}
+}
+}
