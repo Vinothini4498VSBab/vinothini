905d4edab8c6041966b0d0d8f428941e9445add3
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2017-09-12 15:53:17 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n,i,fact=1;
+scanf("%d",&n);
+if(n<0)
+{
+printf("no result");
+}
+else
+{
+for(i=1;i<n;i++)
+{
+fact=fact*i;
+}
+{
+printf("%d",&fact);
+}
+}
+}
