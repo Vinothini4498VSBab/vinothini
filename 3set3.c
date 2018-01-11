12f6af12d5f15bdecb3b9aac280ca20c085f1660
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-01-11 16:49:44 +0530

    Create 3set3.c

---
+++ 3set3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a[n],i,n,min;
+printf("enter the number of elements");
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+scanf("%d",&a[n]);
+min=array[0];
+{
+for(i=0;i<n;i++)
+if(min>a[n])
+{
+min=a[n];
+i++;
+}
+printf("%d",&max);
+}
+}
