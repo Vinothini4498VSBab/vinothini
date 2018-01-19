a6e377036b0056935719c427fe5e9187cbc84107
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-01-19 16:28:47 +0530

    Update 3set5.c

--- 3set5.c
+++ 3set5.c
@@ -7,4 +7,27 @@ printf("enter the number of elements");
 scanf("%d",&n);
 printf("input %d values\n",n);
 for(i=0;i<n;i++)
-scanf(
+scanf("%f",a[X])
+{
+for(i=1;i<=n-1;i++)
+{
+for(j=1;j<=n-i;j++)
+{
+  if(a[i]>a[j])
+  {
+    t=a[i];
+    a[i]=a[j];
+    a[j]=t;
+  }
+}
+}
+}
+  if(n%2==0)
+    median=(a[n/2]+a[n/2=1])/2;
+   else
+     median=(n/2=1)
+  for(i=1;i<=n;i++)
+  {
+  printf("%f",median);
+  }
+}
