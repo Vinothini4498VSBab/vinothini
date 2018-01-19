cb3cf26cb2583e6fa3ca77f392f15b5eb9839af8
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-01-19 17:01:21 +0530

    Create 3set8.c

---
+++ 3set8.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a[i],i,n;
+printf("enter the numberof elements");
+scan("%d",&n)
+printf("elements are")
+for(i=0;i<n;i++)
+scanf("%d",a[i]);
+for(i=0;i<n;i++)
+{
+printf("%d,%d",i,a[i]);
+}
+}
