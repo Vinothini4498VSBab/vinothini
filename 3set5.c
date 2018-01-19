3f00e015014c4a07507fe7c0466784332b6949a0
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-01-19 16:04:28 +0530

    Create 3set5.c

---
+++ 3set5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int n,i,j;
+float median,a[X],t;
+printf("enter the number of elements");
+scanf("%d",&n);
+printf("input %d values\n",n);
+for(i=0;i<n;i++)
+scanf(
