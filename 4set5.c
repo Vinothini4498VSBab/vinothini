ed1878f39c438adcd0b5242ec966793f8b711b72
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-01-21 11:19:40 +0530

    Update 4set5.c

--- 4set5.c
+++ 4set5.c
@@ -2,6 +2,15 @@
 void main()
 {
 char str[400];
+int i,digit=0;
 printf("enter the string");
 scanf("%c",str);
-for
+for(i=0;str[i]!='\0';i++)
+{
+  if(str[i]>=0 && str[i]<=9)
+  {
+    digit++;
+  }
+  printf("%d",&digit);
+}
+}
