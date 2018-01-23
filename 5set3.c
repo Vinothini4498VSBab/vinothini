521ea8977d4960b46a9add05878f540feef179e2
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-01-23 16:57:10 +0530

    Create 5set3.c

---
+++ 5set3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+char s[20],s1[70];
+int i=0,j=o;
+printf("enter first string");
+gets(s);
+printf("enter second string");
+gets(s1);
+while(s[i]!='\0')
+i++;
+while(s1[j]!='\0')
+{
+s=s1;
+j++;
+i++;
+}
+s='\0';
+printf("%s",&s);
+}
