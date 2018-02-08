4987c1e426869bd43ee01f16f9a48aa6dbc3e4fd
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-02-08 09:45:38 +0000

    Rename 6set8.c to 7set8.c

---
+++ 7set8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+   int n,i;
+   printf("enter the number");
+   scanf("%d",&n);
+   if(n%7==0)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+}
+  
