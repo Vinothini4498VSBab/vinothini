70fc15026132eb67cff6491c34231135d49ab679
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-02-13 14:14:50 +0530

    Update 8set6.c

--- 8set6.c
+++ 8set6.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,c=0;
+    int i,n,x=0;
     printf ("Enter a number \n");
     scanf ("%d",&n);
     for (i=1;i<=n;i++)
     {
         if(n%i==0)
-        c=c+1;
+        x=x+1;
     }
-    if (c==2)
+    if (x==2)
     printf ("The number is PRIME");
     else
     printf ("The number is COMPOSITE");
