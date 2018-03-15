466226a55e2098bda27497bf24aa1177f488fbfd
Vinothini4498VSBab <31505112+Vinothini4498VSBab@users.noreply.github.com>
2018-03-15 16:01:16 +0530

    Create p2set1.c

---
+++ p2set1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char s[100];
+    scanf("%s",s);
+    if(s[0]=='s'||s[0]=='S')
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
